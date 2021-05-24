5a20f048656e50af891864b9d346c74a99fb6452
Eric Armstrong <eric.armstrong@uipath.com>
2021-05-24 11:15:20 -0700

    Made Rating perservere to survey Modal

--- interactiveElements.js
+++ interactiveElements.js
@@ -32,7 +32,7 @@ exports.surveyModal = {
 					type: "mrkdwn",
 					text: "Please rate your assigned project on the following scale.\n1. Serious issues that need immediate help\n2. Could be better, project timeline and deliverables are at risk\n3. Going well, no complaints from the customer but not a lot of enthusiasm so far\n4. We’re on schedule and the customer is happy\n5. Ahead of schedule and the customer is already asking us what else can be done with UiPath",
 				},
-			]
+			],
 		},
 		{
 			type: "divider",
@@ -50,7 +50,132 @@ exports.surveyModal = {
 				},
 				options: [
 					{
+						text: {
+							type: "plain_text",
+							text: "1",
+							emoji: true,
+						},
+						value: "*project_1",
+					},
+					{
+						text: {
+							type: "plain_text",
+							text: "2",
+							emoji: true,
+						},
+						value: "*project_2",
+					},
+					{
+						text: {
+							type: "plain_text",
+							text: "3",
+							emoji: true,
+						},
+						value: "*project_3",
+					},
+					{
+						text: {
+							type: "plain_text",
+							text: "4",
+							emoji: true,
+						},
+						value: "*project_4",
+					},
+					{
+						text: {
+							type: "plain_text",
+							text: "5",
+							emoji: true,
+						},
+						value: "*project_5",
+					},
+				],
+				initial_option: {
+					value: "*initialValue",
 					text: {
+						type: "plain_text",
+						text: "*initialText",
+						emoji: true,
+					},
+				},
+			},
+			optional: false,
+			label: {
+				type: "plain_text",
+				text: "How would you rate the status of *project?",
+				emoji: true,
+			},
+		},
+		{
+			type: "input",
+			block_id: "ProjectCommentBlock",
+			label: {
+				type: "plain_text",
+				text: "Please provide feedback for *project.",
+				emoji: true,
+			},
+			element: {
+				type: "plain_text_input",
+				action_id: "CommentAction",
+				multiline: true,
+			},
+			optional: false,
+		},
+	],
+};
+exports.surveyModalDefault = {
+	type: "modal",
+	callback_id: "*surveyURL",
+	title: {
+		type: "plain_text",
+		text: "Project Health Checkup!",
+		emoji: true,
+	},
+	submit: {
+		type: "plain_text",
+		text: "Submit",
+		emoji: true,
+	},
+	close: {
+		type: "plain_text",
+		text: "Cancel",
+		emoji: true,
+	},
+	blocks: [
+		{
+			type: "section",
+			text: {
+				type: "plain_text",
+				text: "We'd love to hear comments from you on *project.",
+				emoji: true,
+			},
+		},
+		{
+			type: "context",
+			elements: [
+				{
+					type: "mrkdwn",
+					text: "Please rate your assigned project on the following scale.\n1. Serious issues that need immediate help\n2. Could be better, project timeline and deliverables are at risk\n3. Going well, no complaints from the customer but not a lot of enthusiasm so far\n4. We’re on schedule and the customer is happy\n5. Ahead of schedule and the customer is already asking us what else can be done with UiPath",
+				},
+			],
+		},
+		{
+			type: "divider",
+		},
+		{
+			type: "input",
+			block_id: "ProjectIssueBlock",
+			element: {
+				type: "static_select",
+				action_id: "ProjectIssueAction",
+				placeholder: {
+					type: "plain_text",
+					text: "Rate your project status",
+					emoji: true,
+				},
+				options: [
+					{
+						text: {
 							type: "plain_text",
 							text: "1",
 							emoji: true,
@@ -115,7 +240,6 @@ exports.surveyModal = {
 		},
 	],
 };
-
 exports.surveyHeader = {
 	blocks: [
 		{
